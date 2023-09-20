aa2536bd7fdafc23fafd22d427783a113721e006
andybeet <22455149+andybeet@users.noreply.github.com>
2023-09-20 13:44:44 -0400

    handles NK species names to have consistent naming

--- R/process_data.R
+++ R/process_data.R
@@ -12,11 +12,11 @@
 process_data <- function(){
 
   # read in portname, lat, lon data
-  ports <- readxl::read_xlsx(here::here("data-raw/ports/Lat Lon Info (ME-FL) 091323.xlsx")) |>
+  ports <- readxl::read_xlsx(here::here("data-raw/ports/Lat Lon Info (ME-FL) 091323.xlsx")) %>%
     dplyr::mutate(MAPNAME = toupper(MAPNAME))
-  ports <- readxl::read_xlsx(here::here("data-raw/ports/Lat Lon Info_Ports (ME-NC) 091323.xlsx")) |>
-    dplyr::mutate(portsinc = stringr::str_replace_all(portsinc,"\\^","")) |>
-    dplyr::select(-GEOTYPE,-areakey) |>
+  ports <- readxl::read_xlsx(here::here("data-raw/ports/Lat Lon Info_Ports (ME-NC) 091323.xlsx")) %>%
+    dplyr::mutate(portsinc = stringr::str_replace_all(portsinc,"\\^","")) %>%
+    dplyr::select(-GEOTYPE,-areakey) %>%
     dplyr::mutate(portsinc = toupper(portsinc))
 
   adddf <- NULL
@@ -32,79 +32,68 @@ process_data <- function(){
     }
   }
 
-  #oracleports <- comlandr::get_ports(channel)$data
 
-
-  data1 <- readRDS(here::here("data-raw/fishing","REVENUEFILE_1996_2007.rds")) |>
-    tibble::as_tibble() |>
+  ## Read in commercial fishing data
+  data1 <- readRDS(here::here("data-raw/fishing","REVENUEFILE_1996_2007.rds")) %>%
+    tibble::as_tibble() %>%
     dplyr::rename(STATE = STATE1,
                   PORT = PORTLND1)
 
-  data2 <- readRDS(here::here("data-raw/fishing","REVENUEFILE_2008_2021.rds")) |>
-    tibble::as_tibble() |>
-    dplyr::mutate(NESPP3 = as.double(NESPP3)) |>
+  data2 <- readRDS(here::here("data-raw/fishing","REVENUEFILE_2008_2021.rds")) %>%
+    tibble::as_tibble()  %>%
+    dplyr::mutate(NESPP3 = as.double(NESPP3))  %>%
     dplyr::rename(STATE = STATE1,
-                  PORT = PORTLND1) |>
+                  PORT = PORTLND1)  %>%
     dplyr::select(-DOCID)
+
+  ## clean fishing data
 
   # need to clean data for duplicate species names. eg. DRUM, BLACK and BLACK DRUM
   # SQUID (LOLIGO), SHRIMP (NK) etc
   # split names by comma and reorder
-  fixdata1 <- data1 |>
-    dplyr::mutate(SPPNM = stringr::str_remove(SPPNM,"\\(BIG\\)")) |>
-    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM," \\(",", ")) |>
-    dplyr::mutate(SPPNM = stringr::str_remove(SPPNM,"\\)")) |>
-    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM,"LOBSTER","AMERICAN LOBSTER")) |>
-    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM,"LOLIGO SQUID","LONGFIN SQUID")) |>
-    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM,"ROSEFISH,BLK BELLIED","BLK BELLIED ROSEFISH")) |>
-    tidyr::separate(col = SPPNM,into = c("last","first"),sep=", ",remove=F) |>
+  fixdata1 <- data1  %>%
+    dplyr::mutate(SPPNM = stringr::str_remove(SPPNM,"\\(BIG\\)"))  %>%
+    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM," \\(",", "))  %>%
+    dplyr::mutate(SPPNM = stringr::str_remove(SPPNM,"\\)"))  %>%
+    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM,"LOBSTER","AMERICAN LOBSTER"))  %>%
+    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM,"LOLIGO SQUID","LONGFIN SQUID"))  %>%
+    dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM,"ROSEFISH,BLK BELLIED","BLK BELLIED ROSEFISH"))  %>%
+    tidyr::separate(col = SPPNM,into = c("last","first"),sep=", ",remove=F)  %>%
     dplyr::mutate(SP = dplyr::case_when(is.na(first) ~ SPPNM,
-                                        TRUE ~ paste(first,last))) |>
+                                        TRUE ~ paste(first,last)))  %>%
     dplyr::select(-first,-last,-SPPNM) |>
     dplyr::rename(SPPNM = SP)
 
 
-    # a <- fixdata1 |>
-    #   dplyr::mutate(SPPNM = stringr::str_remove(SPPNM,"\\(BIG\\)")) |>
-    #   dplyr::mutate(SPPNM = stringr::str_replace_all(SPPNM," \\(",", ")) |>
-    #   dplyr::mutate(SPPNM = stringr::str_remove(SPPNM,"\\)")) |>
-    #   dplyr::distinct(SPPNM,NESPP3)
-    #
-    # length(a$NESPP3)
-    # length(unique(a$NESPP3))
   # join data
-  data <- rbind(fixdata1,data2)
-
-  # a2 <- data |>
-  #   dplyr::select(PORTLANDED, PORT, STATE) |>
-  #   dplyr::distinct() |>
-  #   dplyr::left_join(adddf,by = c("PORT" = "placenm","STATE"="placest"))
-  #
-  # a <- data |>
-  #   dplyr::select(PORTLANDED, PORT, STATE) |>
-  #   dplyr::distinct() |>
-  #   dplyr::left_join(oracleports,by = c("PORT" = "PORTNM","STATE"="STATEABB"))
-  #
-  # missports <- data |>
-  #   dplyr::select(PORTLANDED, PORT, STATE) |>
-  #   dplyr::distinct() |>
-  #   dplyr::left_join(ports,by = c("PORT" = "MAPNAME","STATE"="STATEABBR"))
-  #
-  # missports |>
-  #   dplyr::filter(is.na(PRIMARY_LATITUDE))
+  # change data type
+  # reorder NK species from CLAM NK to NK CLAM
+  data <- rbind(fixdata1,data2) %>%
+    dplyr::mutate(Year = as.numeric(Year),
+                  TRIPID = as.numeric(TRIPID)) %>%
+    tidyr::separate(col = SPPNM,into = c("first","last"),sep=" NK",remove=F) %>%
+    dplyr::mutate(SP = dplyr::case_when(is.na(last) ~ SPPNM,
+                                        TRUE ~ paste("NK",first))) %>%
+    dplyr::select(-first,-last,-SPPNM) %>%
+    dplyr::rename(SPPNM = SP)
 
 
-  rec <-  readRDS(here::here("data-raw/fishing","REVENUEFILE_Neus_Atlantis_rec_1996_2021.rds")) |>
-    tibble::as_tibble() |>
+  ## read in rec landings
+  rec <-  readRDS(here::here("data-raw/fishing","REVENUEFILE_Neus_Atlantis_rec_1996_2021.rds"))  %>%
+    tibble::as_tibble()  %>%
     dplyr::rename(STATE = STATE1,
-                  PORT = PORTLND1) |>
+                  PORT = PORTLND1)  %>%
     dplyr::mutate(Inside = NA,
                   DAY = NA,
                   QTYKEPT = NA,
                   REVENUE = NA,
                   InsideDAS=NA,
-                  nominal_revenue=NA) |>
+                  nominal_revenue=NA)  %>%
     dplyr::relocate(names(data))
+
+  ## clean rec landings
+
+
 
   processedData <- list(data = data, rec = rec)
 
