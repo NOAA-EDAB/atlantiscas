0681ddd9b4a3b77b9414f036454d72ae23480535
andybeet <22455149+andybeet@users.noreply.github.com>
2023-10-13 15:31:16 -0400

    pull tables from CFDBS and clean up a little

--- data-raw/pull_port_tables.r
+++ data-raw/pull_port_tables.r
@@ -2,15 +2,45 @@
 #'
 #' From VTR and CFDBS
 #'
-#'
+#' @param channel A connection object from dbutils::connect_to_data_base`
+#' @param write. Boolean. Write output to file
 #'
 
-pull_port_tables <- function() {
+pull_port_tables <- function(channel,write = F) {
   # pull from vtr
-  porttableVTR <- DBI::dbGetQuery(channel,"select * from VTR.VLPORTSYN")
-  # pull from CFDBS
-  porttableCFDBS <- DBI::dbGetQuery(channel,"select * from CFDBS.PORT")
+  portTableVTR <- DBI::dbGetQuery(channel,"select * from VTR.VLPORTSYN")
+  # this is a mess!
+  if(write) {
+    saveRDS(portTableVTR,here::here("data-raw/portTableVTR"))
+  }
 
-  saveRDS(porttableVTR,here::here("data-raw/portTableVTR"))
-  saveRDS(porttableCFDBS,here::here("data-raw/portTableCFDBS"))
+
+
+  # # pull from CFDBS
+  portTableCFDBS <- DBI::dbGetQuery(channel,"select * from CFDBS.PORT")
+  # clean table
+  # remove all content in parentheses in port name
+
+  portTableCFDBS$PORT <- gsub("\\(\\(","(",portTableCFDBS$PORT)
+
+  portTableCFDBS <- portTableCFDBS %>%
+    tidyr::separate(.,PORTNM,
+                    into=c("NewPORT","extracol"),
+                    sep = "\\(",
+                    fill="right") %>%
+    dplyr::select(-extracol,-PSREGION,-DOC) %>%
+    dplyr::as_tibble() %>%
+    dplyr::rename(PORTID = PORT,
+                  PORT= NewPORT,
+                  STATE=STATEABB) %>%
+    dplyr::distinct()
+
+  #replace OTHER PORT with name of PORT
+  portTableCFDBS$PORT <- gsub("OTHER ","",portTableCFDBS$PORT)
+
+  if(write) {
+    saveRDS(portTableCFDBS,here::here("data-raw/portTableCFDBS"))
+  }
+
+  return(list(portsCFDBS=portTableCFDBS,portsVTR=portTableVTR))
 }
