50b43a3a3f5040eb4bfd504dc36375a1334e672b
andybeet <22455149+andybeet@users.noreply.github.com>
2024-06-27 06:50:29 -0400

    add rec and non neus data to fleet specific data pull

--- R/create_dataset.R
+++ R/create_dataset.R
@@ -4,23 +4,34 @@
 #'
 #' @param fleetType character (scallop, groundfish)
 #'
-#' @return data frame
+#' @return list of 3 data frames
+#' \item{neus}{commercial data from states within NEUS footprint}
+#' \item{outside}{commercial data from states outside NEUS footprint}
+#' \item{rec}{recreational data from all states}
 #'
 #' @export
 
 create_dataset <- function(fleetType) {
 
   cleanData <- readRDS(here::here("data-raw/fishing/processedData.rds"))
-  # just select commercial fleets within the NEUS domain
-  cleanData <- cleanData$data
 
-
+  fleetData <- list()
+  # pick out fleets fo neus states, non neus states (for commercial data), and rec data
   if (tolower(fleetType) == "scallop") {
-    fleetData <- cleanData |>
+    fleetData$neus <- cleanData$data |>
+      dplyr::filter(GEARCAT == "Scallop Gear")
+    fleetData$outsideneus <- cleanData$outside |>
+      dplyr::filter(GEARCAT == "Scallop Gear")
+    fleetData$rec <- cleanData$rec |>
       dplyr::filter(GEARCAT == "Scallop Gear")
 
+
   } else if (tolower(fleetType) == "groundfish") {
-    fleetData <-  cleanData |>
+    fleetData$neus <-  cleanData$data |>
+      dplyr::filter(GEARCAT %in% c("Bottom Trawl","Sink Gillnet"))
+    fleetData$outsideneus <-  cleanData$outside |>
+      dplyr::filter(GEARCAT %in% c("Bottom Trawl","Sink Gillnet"))
+    fleetData$rec <-  cleanData$rec |>
       dplyr::filter(GEARCAT %in% c("Bottom Trawl","Sink Gillnet"))
 
 
