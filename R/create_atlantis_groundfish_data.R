ca0a337e59154eb5ff6fc3f26dd26010301a7c33
andybeet <22455149+andybeet@users.noreply.github.com>
2024-07-24 16:30:05 -0400

    added functions to create the landings and effort data required for atlantis

---
+++ R/create_atlantis_groundfish_data.R
@@ -0,0 +1,31 @@
+#' get processed groundfish data for atlantis use
+#'
+#' Reads in groundfish data and returns effort and landings by box for main ports
+#'
+#' @return data frames
+#' @export
+
+create_atlantis_groundfish_data <- function() {
+
+  lbstotons <- 2204.62
+  gfData <- readRDS(here::here("data/NEGroundfishDataCAMS.rds"))
+
+  # convert to metric tons
+  gf <- gfData$neus |>
+    dplyr::mutate(Year = as.integer(Year)) |>
+    dplyr::mutate(InsideLANDED = InsideLANDED/lbstotons)
+
+  mainPortsIDs = c(240403,240207,220101,240115,420209,240301,320201,350635)
+  associatedPorts = data.frame(main=c(320201,240115,240301),associated=c(320901,240813,240601))
+
+  ports <- unique(c(mainPortsIDs,associatedPorts$associated))
+
+  speciesCodes <- c("COD","HAD","YTF","POL","PLA","WTF","WHK","WIF","RED","HAL","WPF","OPT","WOL")
+
+  effortByBox <- get_effort_landings(sca,ports,associatedPorts,speciesCodes)
+
+
+  return(effortByBox)
+
+}
+
---
+++ R/create_atlantis_scallop_data.R
@@ -0,0 +1,32 @@
+#' get processed scallop data for atlantis use
+#'
+#' Reads in scallop data and returns effort and landings by box for main ports
+#'
+#' @return data frames
+#' @export
+
+create_atlantis_scallop_data <- function() {
+
+  lbstotons <- 2204.62
+  scallopData <- readRDS(here::here("data/scallopDataCAMS.rds"))
+
+  # convert to metric tons
+  sca <- scallopData$neus |>
+    dplyr::mutate(Year = as.integer(Year),
+                  InsideLANDED = InsideLANDED/lbstotons)
+
+  mainPortsIDs = c(240403,330309,490910)
+  associatedPorts = data.frame(main=c(240403,330309,330309,490910,490910),associated=c(242203,331627,330127,490869,490118))
+
+
+  ports <- unique(c(mainPortsIDs,associatedPorts$associated))
+
+  speciesCodes <- "SCA"
+
+  effortByBox <- get_effort_landings(sca,ports,associatedPorts,speciesCodes)
+
+
+  return(effortByBox)
+
+}
+
